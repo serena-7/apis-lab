56aac8d637ae148c2d066b50517ec81d4c4edc64
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-13 14:38:21 -0600

    functionality of swampi requests finished

---
+++ swampi/main.js
@@ -0,0 +1,26 @@
+const getBtn = document.querySelector('button');
+const baseURL = 'https://swapi.dev/api'
+
+const getResidents = (evt) => {
+    console.log('button clicked')
+    axios.get(`${baseURL}/planets?search=alderaan`)
+        .then(res => {
+            const residentsURLs = res.data.results[0].residents;
+            for(let i = 0; i < residentsURLs.length; i++){
+                axios.get(residentsURLs[i])
+                    .then(res => {
+                        const newEle = document.createElement('h2');
+                        newEle.textContent = res.data.name;
+                        document.querySelector('div').appendChild(newEle);
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
+
+getBtn.addEventListener("click", getResidents);
